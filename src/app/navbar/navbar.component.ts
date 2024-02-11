34091bfe6116e8e6640f3982bb4e2a00e5adc360
Gabriel Ferraz <gabrielmaiaferraz@gmail.com>
2024-02-11 11:40:23 -0300

    added buttons and name to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
 import {CommonModule} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     CommonModule,
-    MatToolbar
+    MatToolbar,
+    MatIcon
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
